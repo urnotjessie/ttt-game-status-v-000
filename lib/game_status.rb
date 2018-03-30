5270a5a6a9bfe9f6659b966bc62f234f968899ca
Zhengxiang Gong <jessiegong92@gmail.com>
2018-03-30 19:40:44 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,5 +1,6 @@
 def won?(board)
-
+    if [0, 1, 2, 3, 4, 5, 6, 7, 8].all? { |index| position_taken?(board, index) }
+        return false
     WIN_COMBINATIONS.each do |win_combination|
         win_index_1 = win_combination[0]
         win_index_2 = win_combination[1]
