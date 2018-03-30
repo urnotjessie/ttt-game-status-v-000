692b7d2572aae896c7e665b4f9b0f16484b31492
Zhengxiang Gong <jessiegong92@gmail.com>
2018-03-30 19:36:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,4 +1,5 @@
 def won?(board)
+
     WIN_COMBINATIONS.each do |win_combination|
         win_index_1 = win_combination[0]
         win_index_2 = win_combination[1]
@@ -8,7 +9,9 @@ def won?(board)
         position_2 = board[win_index_2]
         position_3 = board[win_index_3]
 
-        if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        if position_1 == " " && position_2 == " " && position_3 == " "
+            return false
+        elsif position_1 == "X" && position_2 == "X" && position_3 == "X"
             return win_combination
         # elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
         #     return win_combination
