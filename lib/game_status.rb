d882138be8a53f73752f870938d31428cd2f075b
Zhengxiang Gong <jessiegong92@gmail.com>
2018-03-30 19:44:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,6 +12,7 @@ def won?(board)
 
             if position_1 == "X" && position_2 == "X" && position_3 == "X"
                 return win_combination
+                break
             # elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
             #     return win_combination
             else
