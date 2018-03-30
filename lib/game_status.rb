a7d2aa9f86c07b035a85220439fdbd55f2d2cc5c
Zhengxiang Gong <jessiegong92@gmail.com>
2018-03-30 18:50:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,3 +1,13 @@
+def won?(board)
+    WIN_COMBINATIONS.each do |win_combination|
+        win_index_1 = win_combination[0]
+        win_index_2 = win_combination[1]
+        win_index_3 = win_combination[2]
+
+        position_1 = board(win_index_1)
+        position_2 = board(win_index_2)
+        position_3 = board(win_index_3)
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
