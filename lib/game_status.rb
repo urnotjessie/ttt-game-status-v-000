f7e064638a654a725dac4101d3e327ef6793fe75
Zhengxiang Gong <jessiegong92@gmail.com>
2018-03-30 20:39:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,23 +1,17 @@
 def won?(board)
-    # if [0, 1, 2, 3, 4, 5, 6, 7, 8].none? { |index| position_taken?(board, index) }
-    #     return false
-    # else
-        WIN_COMBINATIONS.detect do |win_combination|
-            win_index_1 = win_combination[0]
-            win_index_2 = win_combination[1]
-            win_index_3 = win_combination[2]
+    WIN_COMBINATIONS.detect do |win_combination|
+        win_index_1 = win_combination[0]
+        win_index_2 = win_combination[1]
+        win_index_3 = win_combination[2]
 
-            position_1 = board[win_index_1]
-            position_2 = board[win_index_2]
-            position_3 = board[win_index_3]
+        position_1 = board[win_index_1]
+        position_2 = board[win_index_2]
+        position_3 = board[win_index_3]
 
-            (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
-
-            # elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-            #     return win_combination
-        end
+        (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
     end
 end
+
 
 
 # Helper Method
